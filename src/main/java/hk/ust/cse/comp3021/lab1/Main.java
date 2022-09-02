4360c4d645a44bbc600de8bea79c9cd465e8fd72
rory1132 <mchon@connect.ust.hk>
2022-09-03 06:16:12 +0800

    Created

---
+++ src/main/java/hk/ust/cse/comp3021/lab1/Main.java
@@ -0,0 +1,13 @@
+package hk.ust.cse.comp3021.lab1;
+
+public class Main {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("This is a demo project.");
+    }
+}
+
